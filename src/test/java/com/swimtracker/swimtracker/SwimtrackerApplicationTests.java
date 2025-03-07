6fad278e5b0b8e94ae4acbd55abb12c66def0636
Mikael <mikaelregetz@gmail.com>
2025-03-07 11:02:33 -0300

    first commit

---
+++ src/test/java/com/swimtracker/swimtracker/SwimtrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swimtracker.swimtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwimtrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
